2d59a60a6d6e090d26b243c17207f27ee32bf89a
Beau Yancey <yancey.beau.j@gmail.com>
2024-10-03 21:58:21 -0600

    enhance(test) order pizza

--- tests/pizza.spec.js
+++ tests/pizza.spec.js
@@ -75,3 +75,76 @@ test('logout', async ({page}) => {
   await expect(page.getByRole('link', {name: 'Register'})).toBeVisible();
   await expect(page.getByRole('link', {name: 'DU'})).not.toBeVisible();
 })
+
+test('order pizzas', async ({page}) => {
+  // Arrange
+  await login(page)
+  await page.route('*/**/api/order/menu', async (route) => {
+    if (route.request().method() == 'GET') {
+      const menuRes = [{
+        "id": 1,
+        "title": "Pepperoni",
+        "image": "pizza2.png",
+        "price": 0.0042,
+        "description": "Spicy treat"
+      }]
+      await route.fulfill({json: menuRes})
+    }
+  });
+  await page.route('*/**/api/order', async (route) => {
+    if (route.request().method() == 'POST') {
+      const orderReq = {
+        "items": [
+          {menuId: 1, description: 'Pepperoni', price: 0.0042}
+        ],
+        "storeId": '1',
+        "franchiseId": 1
+      }
+      const orderRes = {
+        "order": {...orderReq, "id": 1},
+        jwt: "abc.def.ghi"
+      }
+      expect(route.request().postDataJSON()).toMatchObject(orderReq)
+      await route.fulfill({json: orderRes})
+    }
+  })
+  await page.route('*/**/api/franchise', async (route) => {
+    if (route.request().method() == 'GET') {
+      const frachiseRes = [{
+        "id": 1,
+        "name": "SuperPie",
+        "stores": [
+          {
+            "id": 1,
+            "name": "Orem"
+          }
+        ]
+      }]
+      await route.fulfill({json: frachiseRes})
+    }
+  });
+
+  // Act - Select Pizzas
+  await page.goto('/')
+  await page.getByRole('link', {name: 'Order'}).click();
+  await page.getByRole('button', {name: 'Pepperoni'}).click();
+
+  // Assert - Select Pizzas
+  await expect(page.getByText('Selected pizzas')).toBeVisible();
+  await expect(page.getByText('Selected pizzas')).toContainText('1');
+
+  // Act - Checkout
+  await page.getByRole('combobox').selectOption('1');
+  await page.getByRole('button', {name: 'Checkout'}).click();
+
+  // Assert - Checkout
+  await expect(page.getByRole('button', {name: 'Pay now'})).toBeVisible()
+  await expect(page.getByRole('table')).toContainText('1 pie')
+  await expect(page.getByRole('table')).toContainText('0.004')
+
+  // Act - Payment
+  await page.getByRole('button', {name: 'Pay now'}).click()
+
+  // Assert - Payment
+  await expect(page.getByText('0.004')).toBeVisible();
+})
